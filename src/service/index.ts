206a6ef2beb3afce839a75a81bf6127cad8a1909
Zyurkalov <Zyurkalov@gmail.com>
2024-06-27 18:14:50 +0500

    fix

--- src/service/index.ts
+++ src/service/index.ts
@@ -35,5 +35,6 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(thunk, liveWcMiddleware),
   });
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
